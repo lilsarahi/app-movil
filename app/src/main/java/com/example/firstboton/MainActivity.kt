a9766f4cd8d3296e1d52346c376023b30c288f73
lilsarahi <sarahi.ortiz.23s@utzmg.edu.mx>
2026-05-11 17:55:35 -0600

    Primer commit

--- app/src/main/java/com/example/firstboton/MainActivity.kt
+++ app/src/main/java/com/example/firstboton/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.firstboton
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textoIng = findViewById<EditText>(R.id.ingtexto)
+        val btnAceptar = findViewById<Button>(R.id.btnAceptar)
+        val lResultado = findViewById<TextView>(R.id.resultado)
+
+        btnAceptar.setOnClickListener {
+
+            val textoIngresado = textoIng.text.toString()
+
+            lResultado.text = textoIngresado
+
+            AlertDialog.Builder(this)
+                .setTitle("¡Éxito!")
+                .setMessage("El texto \"$textoIngresado\" se guardó correctamente.")
+                .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                .show()
         }
     }
 }
